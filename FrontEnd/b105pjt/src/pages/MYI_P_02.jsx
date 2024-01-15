9c286e2a3f80ea55132fc3f75fd75e8141cf9086
조영진 <tl5235566@gmail.com>
2024-01-15 17:07:38 +0900

    feat(MYI_P_02): 텍스트 인풋, 영상 더미데이터, 스크립트 인풋 작성

--- FrontEnd/b105pjt/src/pages/MYI_P_02.jsx
+++ FrontEnd/b105pjt/src/pages/MYI_P_02.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import MyNavbar from "../components/MYI_P_02/myNavbar";
+import { Button, TextField } from "@mui/material";
+import "../assets/css/MYI_P_02.css";
+import SearchSection from "../components/utils/SearchSection";
+import CancelIcon from "@mui/icons-material/Cancel"; // 취소 아이콘
+import CheckCircleIcon from "@mui/icons-material/CheckCircle"; // 등록 (확인) 아이콘
 
 const MYI_P_02 = () => {
   return (
@@ -7,12 +12,43 @@ const MYI_P_02 = () => {
       <MyNavbar></MyNavbar>
 
       {/* insert form */}
-      <div>
-        <div>제목</div>
-        <div>검색창</div>
-        <div>영상</div>
-        <div>스크립트</div>
-        <div>등록</div>
+      <div className="MYI-P-02-Content">
+        <div className="Insert-form">
+          <TextField
+            className="Insert-title"
+            id="filled-basic"
+            label="제목"
+            variant="filled"
+          />
+          <SearchSection className="Insert-search"></SearchSection>
+
+          <img width={"100%"} src="http://placehold.it/720X480" alt="영상" />
+
+          <TextField
+            className="Insert-script"
+            id="filled-multiline-static"
+            label="Multiline"
+            multiline
+            rows={4}
+            defaultValue="Default Value"
+            variant="filled"
+          />
+
+          <div>
+            <div className="Insert-btn-group">
+              <Button variant="outlined" startIcon={<CancelIcon />}>
+                취소
+              </Button>
+              <Button
+                variant="contained"
+                endIcon={<CheckCircleIcon />}
+                color="primary"
+              >
+                등록
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

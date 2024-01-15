683c73668f2cfb71f3b87efdacc7390329222421
박영준 <panddara@naver.com>
2024-01-15 15:54:50 +0900

    feat(MYI_P_01): 아코디언 완성

---
+++ FrontEnd/b105pjt/src/components/MYI_P_01/Accordion.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Accordion as MuiAccordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const Accordion = ({ title, children }) => {
+  return (
+    <MuiAccordion>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography>{title}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography component="div">{children}</Typography>
+      </AccordionDetails>
+    </MuiAccordion>
+  );
+};
+
+export default Accordion;

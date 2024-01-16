81588a106750dab92860056f4ffe9362a27cf1f6
chanhong9784 <chanhong9784@naver.com>
2024-01-17 00:45:37 +0900

    feature(join): 회원가입 기능 일부 구현

--- Backend/Archiview/src/main/java/com/ssafy/archiview/repository/user/UserRepository.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/repository/user/UserRepository.java
@@ -3,5 +3,6 @@ package com.ssafy.archiview.repository.user;
 import com.ssafy.archiview.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
+
 }

1f3cac8494a54e82ea1c863c5a1ee6f3ccb042a7
chanhong9784 <chanhong9784@naver.com>
2024-01-17 00:45:37 +0900

    feature(join): 회원가입 기능 일부 구현

---
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/dto/user/UserDto.java
@@ -0,0 +1,42 @@
+package com.####.archiview.dto.user;
+
+import com.####.archiview.entity.User;
+import com.####.archiview.validation.user.UserEmail;
+import com.####.archiview.validation.user.UserId;
+import com.####.archiview.validation.user.UserName;
+import com.####.archiview.validation.user.UserPassword;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class UserDto {
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class AddRequestDto {
+        @UserId
+        private String id;
+        @UserPassword
+        private String pw;
+        @UserEmail
+        private String email;
+        @UserName
+        private String name;
+        @Builder
+        public AddRequestDto(String id, String pw, String email, String name) {
+            this.id = id;
+            this.pw = pw;
+            this.email = email;
+            this.name = name;
+        }
+
+        public User toEntity() {
+            return User.builder()
+                    .id(id)
+                    .pw(pw)
+                    .email(email)
+                    .name(name)
+                    .build();
+        }
+    }
+}

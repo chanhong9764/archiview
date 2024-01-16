5fd6f49d15682dcb444b7d6e051778c850db9b7e
chanhong9764 <chanhong9784@naver.com>
2024-01-16 13:58:15 +0900

    style(backend): JPA Entitiy 설정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/entity/Reply.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/entity/Reply.java
@@ -1,4 +1,43 @@
 package com.####.archiview.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@DynamicInsert
+@Table(name = "reply") // 답변
 public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_id")
+    @NotNull
+    private String userId;
+
+    @Column(name = "question_id")
+    @NotNull
+    private Integer questionId;
+
+    @ColumnDefault("''")
+    @Column(name = "script")
+    private String script;
+
+    @ColumnDefault("''")
+    @Column(name = "video_url")
+    private String videoUrl;
+
+    @ColumnDefault("''")
+    @Column(name = "thumbnail_url")
+    private String thumbnailUrl;
+
+    @Column(name = "created_at")
+    @NotNull
+    private LocalDateTime createdAt;
 }
--- Backend/Archiview/src/main/java/com/ssafy/archiview/repository/user/UserRepository.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/repository/user/UserRepository.java
@@ -3,5 +3,5 @@ package com.####.archiview.repository.user;
 import com.####.archiview.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository implements JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }

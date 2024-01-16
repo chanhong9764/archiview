5f3865609149692158c117583758792180fcf1f0
insang <jus44556@naver.com>
2024-01-16 13:59:37 +0900

    style(backend): JPA Entity 설정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/entity/CsSubQuestion.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/entity/CsSubQuestion.java
@@ -1,4 +1,24 @@
 package com.####.archiview.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "cs_sub_question")  // 공통/특화 소분류_질문 테이블
 public class CsSubQuestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "question_id")
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer questionId;
+
+    @Column(name = "cs_sub_id")
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer csSubId;
 }
--- Backend/Archiview/src/main/java/com/ssafy/archiview/entity/JobMain.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/entity/JobMain.java
@@ -1,4 +1,18 @@
 package com.####.archiview.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "job_main")  // 직무 대분류 테이블
 public class JobMain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", length = 64)
+    @NotNull
+    private String name;
 }

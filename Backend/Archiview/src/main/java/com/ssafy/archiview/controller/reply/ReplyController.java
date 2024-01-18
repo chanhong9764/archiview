b7dd5b0614ef50e80f863fcea57500f0ad519bef
chanhong9764 <chanhong9784@naver.com>
2024-01-18 20:50:41 +0900

    feat(reply-detail): 답변 상세보기 일부 구현

--- Backend/Archiview/src/main/java/com/ssafy/archiview/controller/reply/ReplyController.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/controller/reply/ReplyController.java
@@ -1,5 +1,6 @@
 package com.ssafy.archiview.controller.reply;
 
+import com.ssafy.archiview.dto.reply.ReplyDto;
 import com.ssafy.archiview.response.code.SuccessCode;
 import com.ssafy.archiview.response.structure.SuccessResponse;
 import com.ssafy.archiview.service.reply.ReplyService;
@@ -13,9 +14,15 @@ import org.springframework.web.bind.annotation.*;
 public class ReplyController {
     private final ReplyService service;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> replyDetail(@PathVariable("id") int id) {
+        ReplyDto.DetailResponseDto responseDto = service.replyDetail(id);
+        return SuccessResponse.createSuccess(SuccessCode.CREATE_REPLY_SUCCESS, responseDto);
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteReply(@PathVariable("id") int id) {
-        service.deleteReply(id);
+    public ResponseEntity<Object> replyDelete(@PathVariable("id") int id) {
+        service.replyDelete(id);
         return SuccessResponse.createSuccess(SuccessCode.DELETE_REPLY_SUCCESS);
     }
 }
--- Backend/Archiview/src/main/java/com/ssafy/archiview/service/reply/ReplyServiceImpl.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/service/reply/ReplyServiceImpl.java
@@ -1,19 +1,51 @@
 package com.ssafy.archiview.service.reply;
 
 import com.ssafy.archiview.dto.reply.ReplyDto;
+import com.ssafy.archiview.entity.Question;
+import com.ssafy.archiview.entity.Reply;
+import com.ssafy.archiview.repository.LikeRepository;
+import com.ssafy.archiview.repository.QuestionRepository;
 import com.ssafy.archiview.repository.ReplyRepository;
 import com.ssafy.archiview.response.code.ErrorCode;
 import com.ssafy.archiview.response.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class ReplyServiceImpl implements ReplyService {
-    private final ReplyRepository repository;
+    private final ReplyRepository replyRepository;
+    private final QuestionRepository questionRepository;
     @Override
-    public void deleteReply(int id) {
-        repository.delete(repository.findById(id)
+    public ReplyDto.DetailResponseDto replyDetail(int id) {
+        // 답변 조회
+        Reply reply = replyRepository.findById(id)
+                .orElseThrow(() -> new RestApiException(ErrorCode.REPLY_NOT_FOUND));
+        // 질문 조회
+        Question question = questionRepository.findById(reply.getQuestionId())
+                .orElseThrow(() -> new RestApiException(ErrorCode.QUESTION_NOT_FOUND));
+
+        // 추천 여부
+        return ReplyDto.DetailResponseDto.builder()
+                .id(reply.getId())
+                .userId(reply.getUser().getId())
+                .questionContent(question.getContent())
+                .script(reply.getScript())
+                .videoUrl(reply.getVideoUrl())
+                .thumbnailUrl(reply.getThumbnailUrl())
+                .likeCount(reply.getLikes().size())
+                .comments(reply.getComments())
+//                .companyName(question.getCompany().getName())
+//                .csList(question.getCsSubQuestionList())
+//                .jobList(question.getJobSubQuestionList())
+                .build();
+    }
+
+    @Override
+    public void replyDelete(int id) {
+        replyRepository.delete(replyRepository.findById(id)
                 .orElseThrow(() -> new RestApiException(ErrorCode.REPLY_NOT_FOUND)));
     }
 }

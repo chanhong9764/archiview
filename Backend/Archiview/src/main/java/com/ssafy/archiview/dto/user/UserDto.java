b8125a09592b7c40f6fda05648128cb02cc00f41
insang <jus44556@naver.com>
2024-01-18 17:09:47 +0900

    refactor(login): 로그인 코드 수정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/dto/user/UserDto.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/dto/user/UserDto.java
@@ -6,10 +6,7 @@ import com.####.archiview.validation.user.UserEmail;
 import com.####.archiview.validation.user.UserId;
 import com.####.archiview.validation.user.UserName;
 import com.####.archiview.validation.user.UserPassword;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -46,6 +43,7 @@ public class UserDto {
     }
 
     @Getter
+    @ToString
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class loginRequestDto{
         @UserId
@@ -70,38 +68,21 @@ public class UserDto {
     @Getter
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class loginResponseDto {
-        @UserId
         private String id;
-        @UserName
         private String name;
-        @UserEmail
         private String email;
         private String profileUrl;
         private String introduce;
-        private LocalDateTime createdAt;
         private Role role;
 
-
-        public loginResponseDto(String id, String name, String email, String profileUrl, String introduce, LocalDateTime createdAt, Role role) {
+        @Builder
+        public loginResponseDto(String id, String name, String email, String profileUrl, String introduce, Role role) {
             this.id = id;
             this.name = name;
             this.email = email;
             this.profileUrl = profileUrl;
             this.introduce = introduce;
-            this.createdAt = createdAt;
             this.role = role;
-        }
-
-        public User toEntity() {
-            return User.builder()
-                    .id(id)
-                    .name(name)
-                    .email(email)
-                    .profileUrl(profileUrl)
-                    .introduce(introduce)
-                    .createdAt(createdAt)
-                    .role(role)
-                    .build();
         }
     }
     @Getter
@@ -112,14 +93,16 @@ public class UserDto {
         private String email;
         private String introduce;
         private String profileUrl;
+        private Role role;
 
         @Builder
-        public DetailResponseDto(String id, String name, String email, String introduce, String profileUrl) {
+        public DetailResponseDto(String id, String name, String email, String introduce, String profileUrl, Role role) {
             this.id = id;
             this.name = name;
             this.email = email;
             this.introduce = introduce;
             this.profileUrl = profileUrl;
+            this.role = role;
         }
     }
 }

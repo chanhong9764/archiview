07b2d4b30fcdc56f79cf50dcd288f2e65133ab39
chanhong9764 <chanhong9784@naver.com>
2024-01-19 14:13:21 +0900

    feat(review): 추천 및 댓글 기능 구현

--- Backend/Archiview/src/main/java/com/ssafy/archiview/service/reply/ReplyService.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/service/reply/ReplyService.java
@@ -2,9 +2,19 @@ package com.####.archiview.service.reply;
 
 import com.####.archiview.dto.reply.ReplyDto;
 
+import java.util.List;
+
 public interface ReplyService {
     // 답변 상세 조회
     ReplyDto.DetailResponseDto replyDetail(ReplyDto.DetailRequestDto requestDto);
     // 답변 삭제
     void replyDelete(int id);
+    // 추천 생성
+    ReplyDto.LikeResponseDto replyLike(ReplyDto.LikeRequestDto requestDto);
+    // 추천 삭제
+    void replyLikeDelete(int id);
+    // 댓글 생성
+    List<ReplyDto.CommentResponseDto> replyComment(ReplyDto.CommentRequestDto requestDto);
+    // 댓글 삭제
+    void replyCommentDelete(int id);
 }

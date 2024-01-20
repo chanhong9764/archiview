8506c0b2a6a5ddf3a136b88d699d4cd16c038b1e
조영진 <tl5235566@gmail.com>
2024-01-20 21:42:38 +0900

    feat(routing): 2차 라우팅 완료

--- FrontEnd/b105pjt/src/components/MYI_P_02/myNavbar.jsx
+++ FrontEnd/b105pjt/src/components/MYI_P_02/myNavbar.jsx
@@ -4,30 +4,54 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const pages = ["질문 등록", "내 질문"];
 
 function MyNavbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // 내 질문 클릭시
+  const handleMyInterview = () => {
+    navigate("/myinterview", { replace: true });
+  };
+
+  // 질문등록 클릭시
+  const handleAddQuestion = () => {
+    navigate("/addquestion", { replace: true });
+  };
+
   return (
     <AppBar style={{ background: "white" }} position="static" sx={{ height: 40, minHeight: 40 }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters sx={{ height: 40, minHeight: 40 }}>
           {/* 큰 사이즈 메뉴 */}
           <Box sx={{ display: "flex", height: "100%" }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{
-                  color: "#222222",
-                  display: "block",
-                  height: 40,
-                  minHeight: 40,
-                  padding: "0 8px",
-                }}
-              >
-                {page}
-              </Button>
-            ))}
+            <Button
+              onClick={handleAddQuestion}
+              sx={{
+                color: "#222222",
+                display: "block",
+                height: 40,
+                minHeight: 40,
+                padding: "0 8px",
+              }}
+            >
+              질문 등록
+            </Button>
+            <Button
+              onClick={handleMyInterview}
+              sx={{
+                color: "#222222",
+                display: "block",
+                height: 40,
+                minHeight: 40,
+                padding: "0 8px",
+              }}
+            >
+              내 질문들
+            </Button>
           </Box>
         </Toolbar>
       </Container>

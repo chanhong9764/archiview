0fa36270c8b9a7471b2ad67a769326cd998373cf
chanhong9784 <chanhong9784@naver.com>
2024-01-24 01:18:22 +0900

    feat(question-search): 질문 검색 수정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/repository/Question/QuestionRepositoryImpl.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/repository/Question/QuestionRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.####.archiview.repository.Question;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.####.archiview.dto.question.QuestionDto;
 import com.####.archiview.entity.*;
@@ -11,16 +13,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class QuestionRepositoryImpl implements QuestionRepositoryCustom{
     private final JPAQueryFactory factory;
+    private final QQuestion question = QQuestion.question;
+    private final QCompany company = QCompany.company;
+    private final QCsSubQuestion csSubQuestion = QCsSubQuestion.csSubQuestion;
+    private final QReply reply = QReply.reply;
     @Override
     public List<Question> searchQuestion(QuestionDto.SearchRequest requestDto) {
         BooleanBuilder builder = new BooleanBuilder();
-        QQuestion question = QQuestion.question;
-        QCompany company = QCompany.company;
-        QReply reply = QReply.reply;
-//
-//        if(requestDto.getUserId().isEmpty()) {
-//            builder.and(reply.user.id.eq(requestDto.getUserId()));
-//        }
+
+        if(requestDto.getUserId().isEmpty()) {
+            builder.and(reply.user.id.eq(requestDto.getUserId()));
+        }
 
         if(!requestDto.getCompanyName().isEmpty()) {
             builder.and(question.company.name.eq(requestDto.getCompanyName()));
@@ -28,7 +31,7 @@ public class QuestionRepositoryImpl implements QuestionRepositoryCustom{
 
         if(!requestDto.getCsList().isEmpty()) {
             for(String cs : requestDto.getCsList()) {
-
+                //tagBuilder.or(csSubQuestion.csSub.name.eq(cs));
             }
         }
 
@@ -36,7 +39,28 @@ public class QuestionRepositoryImpl implements QuestionRepositoryCustom{
                 .from(question)
                 .join(question.company, company)
                 .join(question.replyList, reply)
+                .join(question.csSubQuestionList, csSubQuestion)
                 .where(builder)
                 .fetch();
+    }
+
+    private BooleanExpression userIdEq(String userId) {
+        return userId.isEmpty() ? reply.user.id.eq(userId) : null;
+    }
+
+    private BooleanExpression companyNameEq(String companyName) {
+        return companyName.isEmpty() ? question.company.name.eq(companyName) : null;
+    }
+
+    private BooleanExpression andFilter(String userId, String companyName) {
+        return userIdEq(userId).and(companyNameEq(companyName));
+    }
+
+    private BooleanExpression allFilter(String userId, String companyName) {
+        return andFilter(userId, companyName).and(orFilter());
+    }
+
+    private BooleanExpression orFilter() {
+
     }
 }

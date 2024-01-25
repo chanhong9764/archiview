845be32c03c41a90c22a0e1283c2fb20115be2dd
박영준 <panddara@naver.com>
2024-01-25 13:33:05 +0900

    fix(navbar): 1,3,7,9 수정 완료

--- FrontEnd/b105pjt/src/components/utils/navbar.jsx
+++ FrontEnd/b105pjt/src/components/utils/navbar.jsx
@@ -2,64 +2,73 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import { useState } from "react";
-import { Modal } from "@mui/material";
-import LOG_M_01 from "../../pages/LOG_M_01";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import Logo from "../../assets/img/symbolLogo_Slogun-removebg-preview.png";
-import HOM_M_01 from "../../pages/HOM_M_01";
 
-const pages = ["채용 공고"];
-const settings = ["로그인"];
-const search = ["상세 검색"]
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  maxHeight: 700,
-  overflowY: "auto",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-  borderRadius: "10px",
-};
-
-function NavbarLogin() {
-  const [open, setOpen] = useState(false);
+function Navbar() {
   const navigate = useNavigate();
-  const location = useLocation();
+  const dispatch = useDispatch();
 
-  // LOGO 클릭시
-  const handleRefresh = () => {
-    // 현재 경로로 다시 이동하여 컴포넌트를 리프레시합니다.
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  // 메뉴 여닫기
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  // 메뉴 여닫기
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  // 메뉴 여닫기
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  // 메뉴 여닫기
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  // 로그아웃 클릭시
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
     navigate("/", { replace: true });
   };
 
+  // 로고 클릭시
+  const handleRefresh = () => {
+    navigate("/", { replace: true });
+  };
+
+  // 내 캘린더 클릭시
+  const handleCalendar = () => {
+    navigate("/cal", { replace: true });
+  };
+  
   // 상세 검색 클릭시
   const handleSearch = () => {
-    // 현재 경로로 다시 이동하여 컴포넌트를 리프레시합니다.
     navigate("/search", { replace: true });
   };
 
-  // 메뉴 클릭시
-  const handleCalendar = () => {
-    // 현재 경로로 다시 이동하여 컴포넌트를 리프레시합니다.
-    navigate("/cal", { replace: true });
+  // 면접 관리 클릭시
+  const handleMyInterview = () => {
+    navigate("/myinterview", { replace: true });
   };
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
+  // 면접 관리 클릭시
+  const handleMypage = () => {
+    navigate("/mypage", { replace: true });
   };
 
   return (
@@ -68,6 +77,7 @@ function NavbarLogin() {
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             {/* 큰 사이즈 logo */}
+
             <Box
               onClick={handleRefresh}
               sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
@@ -83,61 +93,115 @@ function NavbarLogin() {
               <img src={Logo} alt="Logo" style={{ height: "40px" }} />
             </Box>
 
-            {/* 좌측 메뉴 */}
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "flex" } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCalendar}
-                  sx={{ my: 2, color: "#222222", display: "block" }}
-                >
-                  {page}
-                </Button>
-              ))}
-              {search.map((search) => (
-                <Button
-                  key={search}
-                  onClick={handleSearch}
-                  sx={{ my: 2, color: "#222222", display: "block" }}
-                >
-                  {search}
-                </Button>
-              ))}
+            {/* 작은 사이즈 메뉴 버튼 */}
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography onClick={handleCalendar} textAlign="center">
+                    채용 공고
+                  </Typography>
+                </MenuItem>
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography onClick={handleSearch} textAlign="center">
+                    상세 검색
+                  </Typography>
+                </MenuItem>
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography onClick={handleMyInterview} textAlign="center">
+                    내 인터뷰
+                  </Typography>
+                </MenuItem>
+              </Menu>
             </Box>
-            
 
-            {/* 우측 메뉴 */}
+            {/* 큰 사이즈 메뉴 */}
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              <Button
+                onClick={handleCalendar}
+                sx={{ my: 2, color: "#222222", display: "block" }}
+              >
+                취업 캘린더
+              </Button>
+              <Button
+                onClick={handleSearch}
+                sx={{ my: 2, color: "#222222", display: "block" }}
+              >
+                상세 검색
+              </Button>
+              <Button
+                onClick={handleMyInterview}
+                sx={{ my: 2, color: "#222222", display: "block" }}
+              >
+                면접 관리
+              </Button>
+            </Box>
+
             <Box sx={{ flexGrow: 0 }}>
-              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "flex" } }}>
-                {settings.map((setting) => (
-                  <Button
-                    key={setting}
-                    onClick={handleOpen}
-                    sx={{ my: 2, color: "#222222", display: "block" }}
-                  >
-                    {setting}
-                  </Button>
-                ))}
-              </Box>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  {/* 프로필 이미지 */}
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography onClick={handleLogout} textAlign="center">
+                    로그아웃
+                  </Typography>
+                </MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography onClick={handleMypage} textAlign="center">
+                    마이페이지
+                  </Typography>
+                </MenuItem>
+              </Menu>
             </Box>
           </Toolbar>
         </Container>
-
-        <div>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="parent-modal-title"
-            aria-describedby="parent-modal-description"
-          >
-            <Box sx={{ ...style, width: 400 }}>
-              <LOG_M_01 close={handleClose}></LOG_M_01>
-            </Box>
-          </Modal>
-        </div>
       </AppBar>
       <Toolbar></Toolbar>
     </div>
   );
 }
-export default NavbarLogin;
+export default Navbar;

761ef43d89d46c26603316caf53e261d6d62facf
조영진 <tl5235566@gmail.com>
2024-01-29 21:48:45 +0900

    feat(WebRTC): 화면 전환 구현

--- FrontEnd/b105pjt/src/components/MYI_P_02/insertForm.jsx
+++ FrontEnd/b105pjt/src/components/MYI_P_02/insertForm.jsx
@@ -18,6 +18,49 @@ import {
 } from "../../api/openViduAPI";
 
 let session;
+let publisher;
+
+const MakeSession = (videoRef) => {
+  const OV = new OpenVidu();
+  session = OV.initSession();
+
+  // 세션 이벤트와 스트림 구독
+  session.on("streamCreated", (event) => {
+    let subscriber = session.subscribe(event.stream, undefined);
+    subscriber.addVideoElement(videoRef.current);
+  });
+
+  getToken(
+    { sessionName: "yourSessionName" + Date.now() }, // API 요청을 위한 매개변수
+    (resp) => {
+      console.log("토큰 받기 성공:", resp.data[0]);
+
+      let token = resp.data[0];
+      session
+        .connect(token, { clientData: "example" })
+        .then(() => {
+          publisher = OV.initPublisher(videoRef.current, {
+            audioSource: undefined,
+            videoSource: undefined,
+            publishAudio: true,
+            publishVideo: true,
+            resolution: "640X480",
+            frameRate: 30,
+            insertMode: "APPEND",
+            mirror: false,
+          });
+
+          session.publish(publisher);
+        })
+        .catch((error) => {
+          console.log("세션 연결 실패:", error);
+        });
+    },
+    (error) => {
+      console.error("토큰 받기 실패:", error);
+    }
+  );
+};
 
 const InsertForm = () => {
   const videoRef = useRef(null); // 비디오 요소 참조를 위한 ref
@@ -25,49 +68,39 @@ const InsertForm = () => {
   const [isRecording, setIsRecording] = useState(false);
 
   useEffect(() => {
-    const OV = new OpenVidu();
-    session = OV.initSession();
-
-    // 세션 이벤트와 스트림 구독
-    session.on("streamCreated", (event) => {
-      let subscriber = session.subscribe(event.stream, undefined);
-      subscriber.addVideoElement(videoRef.current);
-    });
-
-    getToken(
-      { sessionName: "yourSessionName" + Date.now() }, // API 요청을 위한 매개변수
-      (resp) => {
-        console.log("토큰 받기 성공:", resp.data[0]);
-
-        let token = resp.data[0];
-        session
-          .connect(token, { clientData: "example" })
-          .then(() => {
-            const publisher = OV.initPublisher(videoRef.current, {
-              audioSource: undefined,
-              videoSource: undefined,
-              publishAudio: true,
-              publishVideo: true,
-              resolution: "640X480",
-              frameRate: 30,
-              insertMode: "APPEND",
-              mirror: false,
-            });
-
-            session.publish(publisher);
-          })
-          .catch((error) => {
-            console.log("세션 연결 실패:", error);
-          });
-      },
-      (error) => {
-        console.error("토큰 받기 실패:", error);
-      }
-    );
-
     // 컴포넌트 정리
+    MakeSession(videoRef);
+
     return () => {
-      if (session) session.disconnect();
+      if (session) {
+        // 현재 사용자의 연결 해제
+
+        console.log("종료하는 세션ID", session.sessionId);
+        console.log("종료하는 세션ID", session.connection.connectionId);
+        forceDisconnect(
+          {
+            sessionName: session.sessionId,
+            connectionId: session.connection.connectionId,
+          },
+          (resp) => {
+            console.log("forceDisconect success: ", resp);
+          },
+          (err) => {
+            console.log("forceDisconect err: ", err);
+          }
+        );
+
+        // 세션 닫기
+        closeSession(
+          { sessionName: session.sessionId },
+          (resp) => {
+            console.log("closeSession success: ", resp);
+          },
+          (err) => {
+            console.log("closeSession err: ", err);
+          }
+        );
+      }
     };
   }, []);
 
@@ -99,14 +132,19 @@ const InsertForm = () => {
       (resp) => {
         console.log("녹화 종료: ", resp);
         setRecordingURL(
-          "https://i10b105.p.ssafy.io/openvidu/recordings/" +
-            urlSession +
-            "/" +
-            urlSession +
-            ".mp4"
+          "https://i10b105.p.ssafy.io/openvidu/recordings/" + urlSession + "/" + urlSession + ".mp4"
         );
         console.log(recordingURL);
         setIsRecording(false);
+
+        // 세션 및, 퍼블리셔 종료 로직
+        if (publisher) {
+          publisher.stream
+            .getVideoElement()
+            .parentNode.removeChild(publisher.stream.getVideoElement());
+          session.unpublish(publisher);
+        }
+        session.disconnect();
       },
       (error) => {
         console.log("에러 발생: ", error);
@@ -114,44 +152,55 @@ const InsertForm = () => {
     );
   };
 
+  const handleRestartRecording = () => {
+    MakeSession(videoRef);
+    setRecordingURL("");
+  };
+
   return (
     <div>
-      <TextField
-        className="Insert-title"
-        id="filled-basic"
-        label="제목"
-        variant="filled"
-      />
+      <TextField className="Insert-title" id="filled-basic" label="제목" variant="filled" />
 
       <div className="Insert-search">
         <SearchTab></SearchTab>
       </div>
 
       <div>
-        <div ref={videoRef} autoPlay={true} />
         {recordingURL && (
           <div>
-            <video controls src={recordingURL} width="640" height="480"></video>
+            <div>
+              <video controls src={recordingURL} width="640" height="480"></video>
+            </div>
+            <Button variant="contained" color="primary" onClick={handleRestartRecording}>
+              다시 녹화하기
+            </Button>
           </div>
         )}
-        <Button
-          variant="contained"
-          endIcon={<CheckCircleIcon />}
-          color="primary"
-          onClick={handleRecordStart}
-          disabled={isRecording} // 녹화 중에는 버튼 비활성화
-        >
-          등록
-        </Button>
-        <Button
-          variant="contained"
-          endIcon={<CheckCircleIcon />}
-          color="primary"
-          onClick={handleRecordStop}
-          disabled={!isRecording} // 녹화 중이 아니면 버튼 비활성화
-        >
-          취소
-        </Button>
+        {!recordingURL && (
+          <div>
+            <div ref={videoRef} autoPlay={true} />
+            <div>
+              <Button
+                variant="contained"
+                endIcon={<CheckCircleIcon />}
+                color="primary"
+                onClick={handleRecordStart}
+                disabled={isRecording} // 녹화 중에는 버튼 비활성화
+              >
+                녹화 시작
+              </Button>
+              <Button
+                variant="contained"
+                endIcon={<CheckCircleIcon />}
+                color="primary"
+                onClick={handleRecordStop}
+                disabled={!isRecording} // 녹화 중이 아니면 버튼 비활성화
+              >
+                녹화 종료
+              </Button>
+            </div>
+          </div>
+        )}
       </div>
 
       <TextField

648a0c83610d7a294774fda70160c27202ec71ac
박영준 <panddara@naver.com>
2024-01-29 17:34:56 +0900

    fix(MYP_P_02): 저장 누르면 바뀌게

--- FrontEnd/b105pjt/src/components/MYP_P_02/saveButton.jsx
+++ FrontEnd/b105pjt/src/components/MYP_P_02/saveButton.jsx
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { Button, Box } from '@mui/material';
+import ConfirmModal from './confirmModal';
 
-
-// 'onSave' prop 받아서 -> 실행 시 함수 지정 -> 상위 컴포넌트에 정의 -> 'SaveButton'으로 전달 -> 저장 버튼 클릭할 때 호출
 const SaveButton = ({ onSave }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setModalOpen(true);
+  };
+
+  const handleConfirm = () => {
+    setModalOpen(false);
+    onSave(); // 실제 저장 로직을 호출합니다.
+  };
+
+  const handleCancel = () => {
+    setModalOpen(false);
+  };
+
   return (
     <Box sx={{ 
       padding: '20px', 
-        margin: '20px auto', 
+      margin: '20px auto', 
       width: '40%', 
       textAlign: 'center', 
       mt: 1.5, 
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center' 
-      }}>
-      <Button variant="contained" color="primary" onClick={onSave}
-      sx={{ width:'100%',
-       maxWidth: '350px',
-       height:'50px',
-      }}
+    }}>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={handleButtonClick}
+        sx={{ 
+          width:'100%',
+          maxWidth: '350px',
+          height:'50px',
+        }}
       >
         저장
       </Button>
+      <ConfirmModal 
+        open={isModalOpen} 
+        onConfirm={handleConfirm} 
+        onCancel={handleCancel} 
+      />
     </Box>
   );
 };
 
 export default SaveButton;
-

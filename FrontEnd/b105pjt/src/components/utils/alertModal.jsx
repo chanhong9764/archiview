d1d47907a4d60c6a60d5db23d83f6691d18e7700
박영준 <panddara@naver.com>
2024-01-29 17:28:05 +0900

    feat(CAL_M_01): 로그인 창을 띄우기 전 주의 메세지 모달

---
+++ FrontEnd/b105pjt/src/components/utils/alertModal.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Modal, Box, Typography, Button } from "@mui/material";
+import LockOpenIcon from '@mui/icons-material/LockOpen'; // 로그인 아이콘 추가
+
+const AlertModal = ({ open, onClose, onConfirm }) => {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #1976d2', // 테두리 색상 변경
+    borderRadius: '10px', // 테두리 둥글게
+    boxShadow: 24,
+    p: 4,
+    textAlign: 'center', // 텍스트 중앙 정렬
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="alert-modal-title"
+      aria-describedby="alert-modal-description"
+    >
+      <Box sx={style}>
+        <LockOpenIcon color="primary" sx={{ fontSize: 60 }} /> {/* 아이콘 추가 */}
+        <Typography id="alert-modal-title" variant="h6" component="h2" sx={{ mt: 2, mb: 2 }}>
+          로그인이 필요합니다
+        </Typography>
+        <Button variant="contained" color="primary" onClick={onConfirm}>
+          확인
+        </Button>
+      </Box>
+    </Modal>
+  );
+};
+
+export default AlertModal;

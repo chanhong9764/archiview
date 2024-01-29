05dbfd7f29c095b2feed52930e0fed833c6dde02
조영진 <tl5235566@gmail.com>
2024-01-29 21:48:45 +0900

    feat(WebRTC): 화면 전환 구현

--- FrontEnd/b105pjt/src/api/openViduAPI.js
+++ FrontEnd/b105pjt/src/api/openViduAPI.js
@@ -3,73 +3,43 @@ import { openViduAxios } from "../utils/httpCommons";
 const openVidu = openViduAxios();
 
 async function getToken(param, success, fail) {
-  await openVidu
-    .post("api/recording/get-token", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.post("api/recording/get-token", param).then(success).catch(fail);
 }
 
 async function startRecording(param, success, fail) {
-  await openVidu
-    .post("api/recording/recording/start", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.post("api/recording/recording/start", param).then(success).catch(fail);
 }
 
 async function stopRecording(param, success, fail) {
-  await openVidu
-    .post("api/recording/recording/stop", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.post("api/recording/recording/stop", param).then(success).catch(fail);
 }
 
 async function removeUser(param, success, fail) {
-  await openVidu
-    .post("api/recording/recording/remove-user", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.post("api/recording/remove-user", param).then(success).catch(fail);
 }
 
 async function closeSession(param, success, fail) {
-  await openVidu
-    .delete("api/recording/recording/close-session", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.delete("api/recording/close-session", param).then(success).catch(fail);
 }
 
 async function fetchInfo(param, success, fail) {
-  await openVidu
-    .post("api/recording/recording/fetch-info", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.post("api/recording/fetch-info", param).then(success).catch(fail);
 }
 
 async function fetchAll(param, success, fail) {
-  await openVidu
-    .get("api/recording/recording/fetch-all", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.get("api/recording/fetch-all", param).then(success).catch(fail);
 }
 
 async function forceDisconnect(param, success, fail) {
-  await openVidu
-    .delete("api/recording/recording/force-disconnect", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.delete("api/recording/force-disconnect", param).then(success).catch(fail);
 }
 
 async function forceUnpublish(param, success, fail) {
-  await openVidu
-    .delete("api/recording/recording/force-unpublish", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.delete("api/recording/force-unpublish", param).then(success).catch(fail);
 }
 
 async function deleteRecording(param, success, fail) {
-  await openVidu
-    .delete("api/recording/recording/delete", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.delete("api/recording/recording/delete", param).then(success).catch(fail);
 }
 
 async function getRecording(id, param, success, fail) {
@@ -80,10 +50,7 @@ async function getRecording(id, param, success, fail) {
 }
 
 async function listRecordings(param, success, fail) {
-  await openVidu
-    .get("api/recording/recording/list", param)
-    .then(success)
-    .catch(fail);
+  await openVidu.get("api/recording/recording/list", param).then(success).catch(fail);
 }
 
 export {

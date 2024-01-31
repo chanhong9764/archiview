8d67376f4a740cce5a9aba03c1707ef626b7ed44
조영진 <tl5235566@gmail.com>
2024-01-31 17:15:36 +0900

    feat(ADM): 관리자 페이지 작성

---
+++ FrontEnd/b105pjt/src/pages/ADM_P_01.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ManageAccount from "../components/ADM_P_01/manageAccount";
+import ManageGrade from "../components/ADM_P_01/manageGrade";
+
+const ADM_P_01 = () => {
+  return (
+    <div>
+      <ManageAccount></ManageAccount>
+      {/* <ManageGrade></ManageGrade> */}
+    </div>
+  );
+};
+
+export default ADM_P_01;

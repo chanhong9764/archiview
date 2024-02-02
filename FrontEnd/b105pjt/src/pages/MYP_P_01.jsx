10d527549b9ca7eb0e777b8b9a90ad21816a86b8
오철수 <target511@kakao.com>
2024-02-02 16:02:08 +0900

    feat(profile): 프로필 사진 변경 기능 추가
    마이페이지, 회원정보변경 모달에서는 성공적으로 변경된 사진이 갱신되나,
    Navbar에는 갱신되지 않는다.
    핸들러 혹은 redux를 통해서 변경사항이 갱신될 수 있도록 수정할 것.

--- FrontEnd/b105pjt/src/pages/MYP_P_01.jsx
+++ FrontEnd/b105pjt/src/pages/MYP_P_01.jsx
@@ -2,24 +2,7 @@ import React, { useState } from "react";
 import { Container } from "@mui/material";
 import ProfileSection from "../components/MYP_P_01/profileSection";
 
-// dummyProfileData 직접 정의
-// const dummyProfileData = {
-//   code: 200,
-//   message: "회원정보 조회에 성공했습니다.",
-//   data: {
-//     id: "charles",
-//     name: "오철수",
-//     email: "####@naver.com",
-//     introduce: "안녕하세요",
-//   },
-// };
-
-
 const ProfilePage = () => {
-  // const { id, name, email } = dummyProfileData.data;
-  // const [introduce, setIntroduce] = useState(dummyProfileData.data.introduce);
-  // dummyProfileData.data.introduce = introduce;
-
   return (
     <Container
       sx={{

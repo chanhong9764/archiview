658129ceb8f0e56a7226964ceb75ec67c1c7e37b
조영진 <tl5235566@gmail.com>
2024-02-05 21:46:11 +0900

    feat(profile): 프로필 자기소개 작성 완료

--- FrontEnd/b105pjt/src/api/userAPI.js
+++ FrontEnd/b105pjt/src/api/userAPI.js
@@ -68,7 +68,18 @@ async function validPW(token, param, success, fail) {
   await baseURL.post("users/valid-password", param, config).then(success).catch(fail);
 }
 
+async function modifyUserInfo(token, param, success, fail) {
+  console.log(token);
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  await baseURL.patch("users", param, config).then(success).catch(fail);
+}
+
 export {
+  modifyUserInfo,
   signup,
   sendEmail,
   login,

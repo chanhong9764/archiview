616df50e2da9e95dc17daab152caf6e20ecdcbdf
조영진 <tl5235566@gmail.com>
2024-02-06 13:19:00 +0900

    feat(admin): adminAPI 작성

---
+++ FrontEnd/b105pjt/src/api/adminAPI.js
@@ -0,0 +1,16 @@
+import { baseAxios } from "../utils/httpCommons";
+
+const baseURL = baseAxios();
+
+async function getUserList(token, success, fail) {
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+
+  console.log(config);
+  await baseURL.get(`admin/users`, config).then(success).catch(fail);
+}
+
+export { getUserList };

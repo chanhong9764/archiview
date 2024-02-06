3b24ef251f9938e77b9151e85c9cbd2d524f69c6
chanhong9764 <chanhong9784@naver.com>
2024-02-06 16:01:28 +0900

    feat(auth): frontend 페이지 auth 설정

--- FrontEnd/b105pjt/src/components/utils/navbar.jsx
+++ FrontEnd/b105pjt/src/components/utils/navbar.jsx
@@ -23,7 +23,7 @@ import { userDetail } from "../../api/mypageAPI"
 import { Image } from "@mui/icons-material";
 
 function Navbar() {
-  const isAdmin = useSelector((state) => state.isAdmin);
+  const role = useSelector((state) => state.role);
   const accessToken = useSelector((state) => state.accessToken);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -270,7 +270,7 @@ function Navbar() {
                     마이페이지
                   </Typography>
                 </MenuItem>
-                {isAdmin && (
+                {role === "ROLE_ADMIN" && (
                   <MenuItem onClick={handleCloseUserMenu}>
                     <Typography onClick={handleAdminpage} textAlign="center">
                       관리페이지

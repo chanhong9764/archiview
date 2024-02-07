9549ff5e2916631f61febd4699588cb9f09c1607
jus44556 <jus44556@naver.com>
2024-02-08 02:00:11 +0900

    refactor(Redis): Redis 설정파일 수정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/config/RedisConfig.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
@@ -19,5 +20,13 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    // Redis 데이터 조작을 위한 템플릿 클래스, Redis의 데이터를 저장하고 읽는 등의 작업에 사용
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
     }
 }

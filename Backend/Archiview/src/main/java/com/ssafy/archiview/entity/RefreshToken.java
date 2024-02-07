fe820ad75d7feb726a1cd200f9cee7de915cccc0
insang <jus44556@naver.com>
2024-02-07 17:24:12 +0900

    feat(redis): redis 연동 및 refreshToken 저장 구현

---
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/entity/RefreshToken.java
@@ -0,0 +1,22 @@
+package com.####.archiview.entity;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@RedisHash(value = "refreshToken", timeToLive = 1209600)  // 2주 뒤 refreshToken 삭제
+public class RefreshToken {
+    @Id
+    private String id;
+    @Indexed
+    private String refreshToken;
+}

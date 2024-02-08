f40c8554f4f420033c010f9c3d8524d34ce683e0
insang <jus44556@naver.com>
2024-02-08 13:23:46 +0900

    refactor(admin): USER 정지, 강등, 등업 API 수정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/config/RedisConfig.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/config/RedisConfig.java
@@ -1,32 +1,35 @@
-package com.####.archiview.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories
-public class RedisConfig {
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(redisHost, redisPort);
-    }
-
-    @Bean
-    // Redis 데이터 조작을 위한 템플릿 클래스, Redis의 데이터를 저장하고 읽는 등의 작업에 사용
-    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
-    }
-}
+//package com.####.archiview.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//@Configuration
+//@EnableRedisRepositories
+//public class RedisConfig {
+//    @Value("${spring.data.redis.host}")
+//    private String redisHost;
+//    @Value("${spring.data.redis.port}")
+//    private int redisPort;
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory(){
+//        return new LettuceConnectionFactory(redisHost, redisPort);
+//    }
+//
+//    @Bean
+//    // Redis 데이터 조작을 위한 템플릿 클래스, Redis의 데이터를 저장하고 읽는 등의 작업에 사용
+//    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate<String, String> template = new RedisTemplate<>();
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new StringRedisSerializer());
+//        template.setConnectionFactory(redisConnectionFactory);
+//        return template;
+//    }
+//}

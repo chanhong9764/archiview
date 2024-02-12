283b1df7b1a5641ce0298bb1c67bd1ce507967aa
jus44556 <jus44556@naver.com>
2024-02-13 01:36:52 +0900

    refactor(Redis): RefreshToken Redis에 저장하도록 수정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/service/token/TokenService.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/service/token/TokenService.java
@@ -1,8 +1,10 @@
 package com.ssafy.archiview.service.token;
 
 import com.ssafy.archiview.dto.token.TokenDto;
+import com.ssafy.archiview.entity.RefreshToken;
 import com.ssafy.archiview.entity.User;
 import com.ssafy.archiview.jwt.jwtUtil;
+import com.ssafy.archiview.repository.RefreshTokenRepository;
 import com.ssafy.archiview.repository.UserRepository;
 import com.ssafy.archiview.response.code.ErrorCode;
 import com.ssafy.archiview.response.exception.RestApiException;
@@ -13,18 +15,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class TokenService {
     private final UserRepository repository;
+    private final RefreshTokenRepository refreshTokenRepository;
     private final jwtUtil jwtUtil;
     public TokenDto.updateTokenDto updateAccessToken(String accessToken, String refreshToken){
-//        jwtUtil.validateToken(refreshToken);  // RefreshToken 유효성 검사 (만료시 재로그인 해야함)
-
         String userId = jwtUtil.getName(accessToken);
         User user = repository.getById(userId);
-        if(refreshToken.equals(user.getRefreshToken())){
+
+        RefreshToken token = refreshTokenRepository.getById(userId);  // redis에 저장된 RefreshToken Entity
+
+        if(refreshToken.equals(token.getRefreshToken())){
             String updateToken = jwtUtil.createAccessToken(user.getId(), String.valueOf(user.getRole()));
             return new TokenDto.updateTokenDto(updateToken);
         }
         else {
             throw new RestApiException(ErrorCode.UNSUPPORTED_TOKEN);
         }
+
+//        if(refreshToken.equals(user.getRefreshToken())){
+//            String updateToken = jwtUtil.createAccessToken(user.getId(), String.valueOf(user.getRole()));
+//            return new TokenDto.updateTokenDto(updateToken);
+//        }
+//        else {
+//            throw new RestApiException(ErrorCode.UNSUPPORTED_TOKEN);
+//        }
     }
 }

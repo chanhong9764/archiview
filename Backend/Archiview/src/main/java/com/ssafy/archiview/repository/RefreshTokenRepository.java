b7236bc66efc3ab754fdcc3d970bb534f8f00ec6
insang <jus44556@naver.com>
2024-02-13 17:29:15 +0900

    refactor(redis): redis 설정파일 수정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/repository/RefreshTokenRepository.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/repository/RefreshTokenRepository.java
@@ -1,7 +1,6 @@
 package com.####.archiview.repository;
 
 import com.####.archiview.entity.RefreshToken;
-import com.####.archiview.entity.User;
 import com.####.archiview.response.code.ErrorCode;
 import com.####.archiview.response.exception.RestApiException;
 import org.springframework.data.repository.CrudRepository;

c3ede45b0d587e3eebbf1292033e4d9943525945
오철수 <target511@kakao.com>
2024-02-13 10:23:45 +0900

    feat(MyInterview): 질문 등록 시 더미데이터 삭제하고 전달받은 데이터로 요청 보내도록 수정

--- FrontEnd/b105pjt/src/components/MYI_P_02/insertForm.jsx
+++ FrontEnd/b105pjt/src/components/MYI_P_02/insertForm.jsx
@@ -12,21 +12,24 @@ const InsertForm = () => {
   const accessToken = localStorage.getItem("accessToken");
 
   // 실제 데이터로 수정할 때 주석 해제하고 더미데이터 삭제할 것
-  // const [content, setContent] = useState();
-  // const [script, setScript] = useState();
-  // const [companyId, setCompanyId] = useState();
-  // const [csList, setCsList] = useState();
-  // const [jobList, setJobList] = useState();
-  // const [sessionUrl, setSessionUrl] = useState();
+  const [content, setContent] = useState();
+  const [script, setScript] = useState();
+  const [companyId, setCompanyId] = useState();
+  const [csList, setCsList] = useState();
+  const [jobList, setJobList] = useState();
+  const [sessionUrl, setSessionUrl] = useState();
+
+  const [questions, setQuestions] = useState();
+  const [userId, setUserId] = useState();
   // -------------------------------------------------------
 
   // Start - Set Dummy Data
-  const [content, setContent] = useState("테스트 제목");
-  const [script, setScript] = useState("테스트 스크립트");
-  const [companyId, setCompanyId] = useState(3);
-  const [csList, setCsList] = useState(["자기소개", "기업", "기타"]);
-  const [jobList, setJobList] = useState(["게임기획", "경영기획", "광고기획"]);
-  const [sessionUrl, setSessionUrl] = useState();
+  // const [content, setContent] = useState("테스트 제목");
+  // const [script, setScript] = useState("테스트 스크립트");
+  // const [companyId, setCompanyId] = useState(3);
+  // const [csList, setCsList] = useState(["자기소개", "기업", "기타"]);
+  // const [jobList, setJobList] = useState(["게임기획", "경영기획", "광고기획"]);
+  // const [sessionUrl, setSessionUrl] = useState();
   // End - Set Dummy Data
 
   function handlerContent(event) {
@@ -58,7 +61,7 @@ const InsertForm = () => {
         navigate("/myinterview", { replace: true });
       },
       (error) => {
-        console.log("insertForm -> onClickApply | 내 면접 등록 실패");
+        console.log("insertForm -> onClickApply | 내 면접 등록 실패", error);
       }
     );
   }
@@ -78,9 +81,11 @@ const InsertForm = () => {
 
       <div className="Insert-search">
         <SearchTab
+          setQuestions={setQuestions}
           setCompanyId={setCompanyId}
           setCsList={setCsList}
           setJobList={setJobList}
+          userId={userId}
         ></SearchTab>
       </div>
 

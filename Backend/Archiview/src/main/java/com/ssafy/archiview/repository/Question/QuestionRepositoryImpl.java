556533262834ec8624b4f3a90a2df49926ebcce7
chanhong9784 <chanhong9784@naver.com>
2024-07-02 15:49:53 +0900

    refactor(jpa): Query DSL Expression 수정

--- Backend/Archiview/src/main/java/com/ssafy/archiview/repository/Question/QuestionRepositoryImpl.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/repository/Question/QuestionRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.archiview.repository.Question;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.archiview.dto.question.QuestionDto;
 import com.ssafy.archiview.entity.*;
@@ -8,60 +9,77 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 public class QuestionRepositoryImpl implements QuestionRepositoryCustom{
     private final JPAQueryFactory factory;
+    QQuestion question = QQuestion.question;
+    QCompany company = QCompany.company;
+    QCsSubQuestion csSubQuestion = QCsSubQuestion.csSubQuestion;
+    QJobSubQuestion jobSubQuestion = QJobSubQuestion.jobSubQuestion;
+    QReply reply = QReply.reply;
     @Override
-    public List<Question> searchQuestion(QuestionDto.SearchRequest requestDto) {
-        QQuestion question = QQuestion.question;
-        QCompany company = QCompany.company;
-        QCsSubQuestion csSubQuestion = QCsSubQuestion.csSubQuestion;
-        QJobSubQuestion jobSubQuestion = QJobSubQuestion.jobSubQuestion;
-        QReply reply = QReply.reply;
-
-        BooleanBuilder andBuilder = new BooleanBuilder();
-        BooleanBuilder orBuilder = new BooleanBuilder();
-
+    public List<Question> searchQuestion(QuestionDto.SearchRequest requestDto){
         final int SizeConstant = 10;
         final int pgno = requestDto.getPgno() * SizeConstant - SizeConstant;
 
-        if(StringUtils.hasText(requestDto.getUserId())) {
-            andBuilder.and(reply.user.id.eq(requestDto.getUserId()));
-        } else {
-            andBuilder.and(reply.user.role.eq(Role.ROLE_MEMBER));
-        }
-
-        if(StringUtils.hasText(requestDto.getCompanyName())) {
-            andBuilder.and(question.company.name.eq(requestDto.getCompanyName()));
-        }
-
-        if(!requestDto.getCsList().isEmpty()) {
-            for(String cs : requestDto.getCsList()) {
-                if(StringUtils.hasText(cs)) {
-                    orBuilder.or(csSubQuestion.csSub.name.eq(cs));
-                }
-            }
-        }
-
-        if(!requestDto.getJobList().isEmpty()) {
-            for(String js : requestDto.getJobList()) {
-                if(StringUtils.hasText(js)) {
-                    orBuilder.or(jobSubQuestion.jobSub.name.eq(js));
-                }
-            }
-        }
-
         return factory.select(question)
                 .from(question)
-                .join(question.company, company)
-                .join(question.replyList, reply)
+                .leftJoin(question.company, company)
+                .leftJoin(question.replyList, reply)
                 .leftJoin(question.csSubQuestionList, csSubQuestion)
                 .leftJoin(question.jobSubQuestionList, jobSubQuestion)
-                .where(andBuilder.and(orBuilder))
+                .where(searchAndFilter(requestDto)
+                        .and(tagOrFilter(requestDto.getCsList(), requestDto.getJobList())))
                 .distinct()
+                .fetchJoin()
                 .offset(pgno)
                 .limit(SizeConstant)
                 .fetch();
+    }
+
+    private BooleanBuilder searchAndFilter(QuestionDto.SearchRequest requestDto) {
+        return (StringUtils.hasText(requestDto.getUserId()) ? userIdEq(requestDto.getUserId()) : userRoleEq())
+                .and(companyNameEq(requestDto.getCompanyName()));
+    }
+
+    private BooleanBuilder tagOrFilter(List<String> csList, List<String> jobList) {
+        BooleanBuilder builder = new BooleanBuilder();
+        for(String cs : csList) {
+            builder.or(csSubNameEq(cs));
+        }
+        for(String job : jobList) {
+            builder.or(jobSubNameEq(job));
+        }
+        return builder;
+    }
+
+    private BooleanBuilder userIdEq(String userId) {
+        return nullSafeBooleanBuilder(() -> reply.user.id.eq(userId));
+    }
+
+    private BooleanBuilder userRoleEq() {
+        return nullSafeBooleanBuilder(() ->  reply.user.role.eq(Role.ROLE_MEMBER));
+    }
+
+    private BooleanBuilder companyNameEq(String companyName) {
+        return nullSafeBooleanBuilder(() ->  question.company.name.eq(companyName));
+    }
+
+    private BooleanBuilder csSubNameEq(String csSubName) {
+        return nullSafeBooleanBuilder(() -> csSubQuestion.csSub.name.eq(csSubName));
+    }
+
+    private BooleanBuilder jobSubNameEq(String jobSubName) {
+        return nullSafeBooleanBuilder(() -> jobSubQuestion.jobSub.name.eq(jobSubName));
+    }
+
+    private BooleanBuilder nullSafeBooleanBuilder(Supplier<BooleanExpression> supplier) {
+        try {
+            return new BooleanBuilder(supplier.get());
+        } catch (IllegalArgumentException e) {
+            return new BooleanBuilder();
+        }
     }
 }
--- Backend/Archiview/src/main/java/com/ssafy/archiview/repository/Recruit/RecruitRepositoryImpl.java
+++ Backend/Archiview/src/main/java/com/ssafy/archiview/repository/Recruit/RecruitRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.archiview.repository.Recruit;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -8,34 +9,54 @@ import com.ssafy.archiview.dto.question.QuestionDto;
 import com.ssafy.archiview.dto.recruit.RecruitDto;
 import com.ssafy.archiview.entity.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 public class RecruitRepositoryImpl implements RecruitRepositoryCustom {
     private final JPAQueryFactory factory;
+    QRecruit recruit = QRecruit.recruit;
+    QCompany company = QCompany.company;
     @Override
     public List<RecruitDto.DetailListResponseDto> searchAll(RecruitDto.DetailListRequestDto requestDto) {
-        BooleanBuilder builder = new BooleanBuilder();
-        QRecruit recruit = QRecruit.recruit;
-        QCompany company = QCompany.company;
-
-        StringExpression start = Expressions.stringTemplate("FUNCTION('DATE_FORMAT', {0}, '%Y-%m')", recruit.start);
-        StringExpression end = Expressions.stringTemplate("FUNCTION('DATE_FORMAT', {0}, '%Y-%m')", recruit.end);
-
-        builder.and(start.eq(requestDto.getDate()).or(end.eq(requestDto.getDate())));
-
-        if(requestDto.getCompanyId() != 0) {
-            builder.and(recruit.company.id.eq(requestDto.getCompanyId()));
-        }
-
         return factory
                 .selectFrom(recruit)
                 .leftJoin(recruit.company, company)
                 .fetchJoin()
-                .where(builder)
+                .where(
+                        dateEq(requestDto.getDate())
+                                .and(companyIdEq(requestDto.getCompanyId()))
+                )
                 .fetch().stream()
                 .map(Recruit::toDetailListDto)
                 .toList();
+    }
+
+    private BooleanBuilder dateEq(String date) {
+        return startEq(date).or(endEq(date));
+    }
+
+    private BooleanBuilder companyIdEq(Integer companyId) {
+        return nullSafeBooleanBuilder(() -> recruit.company.id.eq(companyId));
+    }
+
+    private BooleanBuilder startEq(String date) {
+        StringExpression start = Expressions.stringTemplate("FUNCTION('DATE_FORMAT', {0}, '%Y-%m')", recruit.start);
+        return nullSafeBooleanBuilder(() -> start.eq(date));
+    }
+
+    private BooleanBuilder endEq(String date) {
+        StringExpression end = Expressions.stringTemplate("FUNCTION('DATE_FORMAT', {0}, '%Y-%m')", recruit.end);
+        return nullSafeBooleanBuilder(() -> end.eq(date));
+    }
+
+    private BooleanBuilder nullSafeBooleanBuilder(Supplier<BooleanExpression> supplier) {
+        try {
+            return new BooleanBuilder(supplier.get());
+        } catch (IllegalArgumentException e) {
+            return new BooleanBuilder();
+        }
     }
 }
